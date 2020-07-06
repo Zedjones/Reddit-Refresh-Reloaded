13129068c5b32c56ce76f9262a49c817d6add8c0
Zedjones <dojoman19@gmail.com>
2020-07-05 23:00:09 -0400

    Added extractor for bearer token

--- src/routes.rs
+++ src/routes.rs
@@ -1,13 +1,22 @@
 use crate::graphql::schema::Schema;
 use actix_web::{get, post, web, HttpResponse, Result};
+use actix_web_httpauth::extractors::bearer::BearerAuth;
 use async_graphql::http::{playground_source, GraphQLPlaygroundConfig};
 use async_graphql_actix_web::{GQLRequest, GQLResponse};
 
 // TODO: Take in the JWT token here, and then call `.data(token)` on the builder
 // to provide the token as context to the individual query
 #[post("/graphql")]
-pub(crate) async fn graphql(schema: web::Data<Schema>, req: GQLRequest) -> GQLResponse {
-    req.into_inner().execute(&schema).await.into()
+pub(crate) async fn graphql(
+    auth: BearerAuth,
+    schema: web::Data<Schema>,
+    req: GQLRequest,
+) -> GQLResponse {
+    req.into_inner()
+        .data(String::from(auth.token()))
+        .execute(&schema)
+        .await
+        .into()
 }
 
 #[get("/graphql")]
