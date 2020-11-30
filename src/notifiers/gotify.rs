aa9b8f92e706cdb7c417a9d8e67c4a03a03826b0
Zedjones <dojoman19@gmail.com>
2020-11-29 22:54:30 -0500

    Added initial Gotify notifier

---
+++ src/notifiers/gotify.rs
@@ -0,0 +1,27 @@
+// Notifier to write to a file
+use async_trait::async_trait;
+use reqwest::Client;
+
+use super::Notifier;
+
+pub(crate) struct GotifyNotifier {
+    server_url: String,
+    token: String,
+    priority: Option<u64>,
+    client: Client,
+}
+
+#[async_trait]
+impl Notifier for GotifyNotifier {
+    async fn notify(&self, result: crate::db::Result) {
+        let mut req = self
+            .client
+            .post(&format!("{}/message?token={}", self.server_url, self.token))
+            .header("title", result.title)
+            .header("message", result.permalink);
+        if let Some(priority) = self.priority {
+            req = req.header("priority", priority);
+        }
+        req.send().await.unwrap();
+    }
+}
--- src/notifiers/mod.rs
+++ src/notifiers/mod.rs
@@ -1,1 +1,8 @@
-pub trait Notifier {}
+use crate::db::result::Result;
+use async_trait::async_trait;
+mod gotify;
+
+#[async_trait]
+pub(crate) trait Notifier {
+    async fn notify(&self, result: Result);
+}
